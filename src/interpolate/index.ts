a8289e4df470722c2c0a63302957151bafde8f0b
wuxingjie <592208167@qq.com>
2023-08-13 23:38:33 +0800

    新增插值函数

--- src/interpolate/index.ts
+++ src/interpolate/index.ts
@@ -1,6 +1,6 @@
 import { constFunc } from '../utils/functional';
 import { Stream } from '../utils/stream';
-import { isNotNull, isNull, isNumeric } from '../utils/types';
+import { isNull, isNumeric } from '../utils/types';
 
 function number(a: number, b: number): (t: number) => number {
     return (t: number) => {
@@ -15,11 +15,11 @@ function numberRound(a: number, b: number): (t: number) => number {
     };
 }
 
-const numRegExp1 = /[-+]?(?:\d+\.?\d*|\.?\d+)(?:[eE][-+]?\d+)?/g;
+const numRegExp = /[-+]?(?:\d+\.?\d*|\.?\d+)(?:[eE][-+]?\d+)?/g;
 const numRegExpSplit = /([-+]?(?:\d+\.?\d*|\.?\d+)(?:[eE][-+]?\d+)?)/g;
 function string(a: string, b: string): (t: number) => string {
-    numRegExp1.lastIndex = numRegExpSplit.lastIndex = 0;
-    const aNumbers = Stream.generate(() => numRegExp1.exec(a))
+    numRegExp.lastIndex = numRegExpSplit.lastIndex = 0;
+    const aNumbers = Stream.generate(() => numRegExp.exec(a))
         .takeWhile(isNull)
         .filterNotNull()
         .map((r) => Number(r[0]))
@@ -42,4 +42,44 @@ function string(a: string, b: string): (t: number) => string {
         return tokenFunctions.map((f) => f(t)).join('');
     };
 }
-export { number as interpolateNumber, numberRound as interpolateNumberRound, string as interpolateString };
+
+/**
+ * 注意每次返回的Date为同一对象
+ * @param a
+ * @param b
+ */
+function date(a: Date, b: Date): (t: number) => Date {
+    const interp = number(+a, +b);
+    const d = new Date();
+    return (t: number): Date => {
+        d.setTime(interp(t));
+        return d;
+    };
+}
+
+/**
+ * 注意每次调用返回的是同一数组对象
+ * @param a
+ * @param b
+ */
+function numberArray(a: number[], b: number[]): (t: number) => number[] {
+    let i = 0;
+    //  每个元素的插值器函数
+    const funcs = Stream.from(b)
+        .takeWhile(() => a[i++] === undefined)
+        .map((e, i) => number(a[i], e))
+        .toArray();
+    const res = b.slice();
+    return (t: number): number[] => {
+        funcs.forEach((f, i) => (res[i] = f(t)));
+        return res;
+    };
+}
+
+export {
+    number as interpolateNumber,
+    numberRound as interpolateNumberRound,
+    string as interpolateString,
+    date as interpolateDate,
+    numberArray as interpolateNumberArray,
+};
